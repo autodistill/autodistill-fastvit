fe7c6e8db5728c3d394067a7c8f5ad2d23487940
James Gallagher <jamesg@jamesg.blog>
2023-12-06 09:29:32 +0000

    use load_image, divide probs by 100 to get 0-1 probs

--- autodistill_fastvit/__init__.py
+++ autodistill_fastvit/__init__.py
@@ -1,3 +1,3 @@
-from autodistill_fastvit.fastvit import FastViT, FASTVIT_IMAGENET_1K_CLASSES
+from autodistill_fastvit.fastvit import FASTVIT_IMAGENET_1K_CLASSES, FastViT
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
